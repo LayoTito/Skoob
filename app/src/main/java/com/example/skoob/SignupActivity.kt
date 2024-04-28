ab3f484a9c5031971bcc65fb66fe4966a43ba13e
unknown <layo.llts@gmail.com>
2024-04-28 11:32:24 -0300

    All files renamed, less: Login activities, back-end WelcomeActivity

--- app/src/main/java/com/example/skoob/SignupActivity.kt
+++ app/src/main/java/com/example/skoob/SignupActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_signup : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/example/skoob/WelcomeActivity.kt
+++ app/src/main/java/com/example/skoob/WelcomeActivity.kt
@@ -1,20 +1,37 @@
 package com.example.skoob
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_review : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_review)
+        setContentView(R.layout.activity_welcome)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    fun toSignUpScreen() {
+
+        val intentSignUp = Intent(this, SignupActivity::class.java)
+        startActivity(intentSignUp)
+
+    }
+
+    fun toLogInScreen() {
+
+        val intentLogIn = Intent(this, LoginActivity::class.java)
+        startActivity(intentLogIn)
+    }
+
 }
