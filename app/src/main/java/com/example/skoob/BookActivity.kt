be396f9fd4cda223af8c9b49bc37f16d1bb8b5ff
ihopes11 <aldairmarinojunior@gmail.com>
2024-04-30 19:53:00 -0300

    19:52 4/30

--- app/src/main/java/com/example/skoob/BookActivity.kt
+++ app/src/main/java/com/example/skoob/BookActivity.kt
@@ -1,12 +1,18 @@
 package com.example.skoob
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class BookActivity : AppCompatActivity() {
+
+    private lateinit var bridgeBookAdd: ImageButton
+    private lateinit var bridgeBookUser: ImageView
+    private lateinit var
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
