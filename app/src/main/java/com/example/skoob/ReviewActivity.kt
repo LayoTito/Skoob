ff08e1edc04fd9943e81313fb6eb2c25b550b637
VinneCarv <viniciuscarvalhoyee@gmail.com>
2024-04-30 23:01:50 -0300

    review: basic backend strucure

--- app/src/main/java/com/example/skoob/ReviewActivity.kt
+++ app/src/main/java/com/example/skoob/ReviewActivity.kt
@@ -1,6 +1,10 @@
 package com.example.skoob
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,7 +12,13 @@ import androidx.core.view.WindowInsetsCompat
 
 class ReviewActivity : AppCompatActivity() {
 
-
+    private lateinit var bridgeReviewReturn: ImageButton
+    private lateinit var bridgeReviewComment: EditText
+    private lateinit var bridgeReviewRate1: Button
+    private lateinit var bridgeReviewRate2: Button
+    private lateinit var bridgeReviewRate3: Button
+    private lateinit var bridgeReviewRate4: Button
+    private lateinit var bridgeReviewRate5: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,5 +28,26 @@ class ReviewActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        bridgeReviewReturn = findViewById(R.id.reviewButtonReturn)
+        bridgeReviewComment = findViewById(R.id.reviewEditReview)
+        bridgeReviewRate1 = findViewById(R.id.reviewButtonRate_1)
+        bridgeReviewRate2 = findViewById(R.id.reviewButtonRate_2)
+        bridgeReviewRate3 = findViewById(R.id.reviewButtonRate_3)
+        bridgeReviewRate4 = findViewById(R.id.reviewButtonRate_4)
+        bridgeReviewRate5 = findViewById(R.id.reviewButtonRate_5)
+
+
+        bridgeReviewReturn.setOnClickListener{
+
+            toHomeScreen()
+
+        }
+    }
+    private fun toHomeScreen() {
+
+        val intentHome = Intent(this, HomeActivity::class.java)
+        startActivity(intentHome)
+
     }
 }
